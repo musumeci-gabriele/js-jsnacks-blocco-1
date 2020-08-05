66d1b4e0eb5049c75e23e0ca540f698e7cda83a3
Gabriele-Musumeci <musumeci.gabriele@hotmail.com>
2020-08-05 15:35:09 +0200

    start blocco 3

---
+++ snack-3/script.js
@@ -0,0 +1,11 @@
+// INSERISCI UN NUMERO
+var numOne = parseInt(prompt("Inserisci un numero :"));
+
+// SE IL NUMERO E' PARI VERRA'STAMPATO
+if (numOne % 2 ==0) {
+  console.log(numOne);
+}
+// ALTRIMENTI AL NUMERO VERRA' SOMMATO UNO
+else {
+  console.log(numOne + 1);
+}
